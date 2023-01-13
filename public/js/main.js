01c4dd894b0b0239aefa39a29b5f308412a8b2a5
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-13 14:46:36 +0100

    Parcours des tableaux et ajout des propriétés de l'objet dans les éléments de myElements

--- public/js/main.js
+++ public/js/main.js
@@ -16,9 +16,18 @@ let myElements = [div.firstElementChild, div.lastElementChild];
 let tableau = Object.keys(myObject).map(function(cle){
     return [cle, myObject[cle]];
 });
+console.log(tableau);
 
 // ### 3. Créer une boucle qui va parcourir toutes les propriétés contenu dans ton object et qui pour chacune d'elle fait un console.log de la propriété
 // ### 4. Efface ton console.log, trouve un moyen d'aller dans ton objet récupéré la valeur de chaque propriétés et fait un console log de ce que cela retourne
 // ### 5. Rajoute un count(compteur ou index comme tu l'entends) dans ta boucle
+let count = 0;
+for(let i = 0; i < tableau.length; i++){
+    console.log(tableau[i][0]);
+    console.log(tableau[i][1]);
+    count++;
+}
 // ### 6. Sert toi de ce count/index pour parcourir ton tableau myElement dans ta boucle et d'y insirer ce qu'on te retourne a l'énoncé 4
-
+for(let i = 0; i < count; i++){
+    myElements[i].innerText = tableau[i];
+}
